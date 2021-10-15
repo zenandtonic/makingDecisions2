8dfd8cfe2dd7e28905eba339b59accaf3b7b46a2
Michael Bodin <92333134+zenandtonic@users.noreply.github.com>
2021-10-15 02:14:35 -0500

    Add files via upload
    at problem #10

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -99,7 +99,11 @@ console.log(someNum)
 // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
-//Code Here
+if (myFavoriteNumbers.length < 7) {
+  console.log("There are not enough elements in this array")
+} else if (myFavoriteNumbers.length > 7) {
+  let someNum = myFavoriteNumbers[6]
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -110,7 +114,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
-//Code Here
+for (let i = 0; i < listOfNumbers.length; i++) {
+  if (listOfNumbers[i] % 3 == 0) {
+    console.log(listOfNumbers[i] + " is divisible by 3")
+  }
+}
 
 
 ////////// PROBLEM 9 //////////
@@ -122,7 +130,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
 */
 
-//Code Here
+for (var i = letters.length - 1; i >= 0; i--) {
+  console.log(letters[i]);
+}
 
 
 ////////// Advanced Problems //////////
